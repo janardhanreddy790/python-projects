815ad218ca57611f06f5dad3c6bde5a3b996a55d
Janardhan Reddy Nagam <nagamjanardhanredy@gmail.com>
2025-11-09 19:58:59 +0100

    if usage

---
+++ conditional-statements/ifUsage.py
@@ -0,0 +1,9 @@
+# 'if' condition is used to define a rule and based on that take decision what you want to perform.
+
+# Example:
+i = 10
+
+if i == 10:  # prints true only if the variable 'i' value value is 10 otherwise it will print false
+    print("true")
+else:
+    print("false")
